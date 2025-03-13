70ba449a05b57207e0d6eb525c9ae03ade21fdaf
terehov <terehovivan767@gmail.com>
2025-03-13 15:20:11 +0300

    Creating a form in HTML

---
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -0,0 +1,15 @@
+import debug_toolbar
+
+from django.contrib import admin
+from django.urls import include, path
+
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('homepage.urls')),
+    path('about/', include('about.urls')),
+    path('ice_cream/', include('ice_cream.urls')),
+    path('contest/', include('contest.urls')),
+    path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
+]
